eaafcf9137cef6560f7d802958c28f1a78a2490f
Anuj Shrivastava <anujshri1729@gmail.com>
2024-11-14 20:22:08 +0530

    First Commit

--- src/main/java/com/rcoem/devops/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/rcoem/devops/exceptions/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import java.util.NoSuchElementException; 
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
